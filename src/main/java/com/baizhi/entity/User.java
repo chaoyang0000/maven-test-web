2769e3b06afe6ea9a92fc54732054356b50b39e2
chaoyang0000 <47848831+chaoyang0000@users.noreply.github.com>
2019-02-22 20:29:49 +0800

    把尸体类的有参构造方法删了

--- src/main/java/com/baizhi/entity/User.java
+++ src/main/java/com/baizhi/entity/User.java
@@ -4,15 +4,11 @@ public class User {
     private Integer id;
     private String name;
     private String pwd;
-
+    
     public User() {
     }
 
-    public User(Integer id, String name, String pwd) {
-        this.id = id;
-        this.name = name;
-        this.pwd = pwd;
-    }
+    
 
     public Integer getId() {
         return id;
